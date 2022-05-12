215c9608c2a8845529c8a6a60d1d95e2135e4260
Jefid <99925114+Jefid@users.noreply.github.com>
2022-05-12 16:14:15 -0400

    restructure

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 
 // GET routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
